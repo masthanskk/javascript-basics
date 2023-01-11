3a7bdbf6f586f49358fc408e27687f489bff5ed7
Shami <skmasthan>
2023-01-11 14:44:56 +0530

    no of ways to create functions

---
+++ javascript-training/7-functions/006-no-of-ways-to-create-functions.js
@@ -0,0 +1,26 @@
+
+
+// 1.function Expression
+
+function add (a,b){ return a+b;}
+
+// 2.function Declaration.
+
+const subtrction = function (a,b){
+    return a - b;
+}
+
+
+// 3.Arrow function (ES6 latest runing Version (ES6 - > EcmaScript 6) ) Arrow function treated as Expression function 
+
+const Multiplication = (a,b)=>{
+    return a * b;
+}
+
+                                  // `45` and `78` are Values
+                                  // or also known as passing arguments to add function
+
+ const addition = (24,44);
+ 
+ console.log(`Addition value is ${addition}`);
+ 
